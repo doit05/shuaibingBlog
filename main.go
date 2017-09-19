dc7d9511c088ac6f39209af53c50ae0d1a544fc7
zhanshuaibing <zhanshuaibing@douyu.tv>
2017-09-19 20:18:40 +0800

    test

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"mysite/helper/apicode"
 	"shuaibingBlog/helper"
@@ -41,24 +42,26 @@ func main() {
 	//	  "foo":  "bar",
 	//	  "manu": "123",
 	//}))
-	//authorized := r.Group("/", gin.BasicAuth(gin.Accounts{
-	//	"foo":  "bar", // user:foo password:bar
-	//	"manu": "123", // user:manu password:123
-	//}))
-	//
-	//authorized.POST("admin", func(c *gin.Context) {
-	//	user := c.MustGet(gin.AuthUserKey).(string)
-	//
-	//	// Parse JSON
-	//	var json struct {
-	//		Value string `json:"value" binding:"required"`
-	//	}
-	//
-	//	if c.Bind(&json) == nil {
-	//		DB[user] = json.Value
-	//		c.JSON(200, gin.H{"status": "ok"})
-	//	}
-	//})
+	authorized := r.Group("/", gin.BasicAuth(gin.Accounts{
+		"foo":  "bar", // user:foo password:bar
+		"manu": "123", // user:manu password:123
+	}))
+
+	authorized.POST("admin", func(c *gin.Context) {
+		test := c.MustGet(gin.AuthUserKey).(string)
+
+		// Parse JSON
+		var json struct {
+			Value string `json:"value" binding:"required"`
+		}
+
+		fmt.Println(c.Bind(&json))
+
+		if c.Bind(&json) == nil {
+			c.JSON(200, gin.H{"status": json.Value})
+		}
+		fmt.Println(test)
+	})
 
 	// Listen and Server in 0.0.0.0:8080
 	r.Run(":8080")
