10636352da6646fff80bfd867ce9cf16bf89e2c4
zhanshuaibing <zhanshuaibing@douyu.tv>
2017-10-30 16:33:12 +0800

    添加toml配置文件

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/robvdl/pongo2gin"
+	"shuaibingBlog/helper"
+	_ "shuaibingBlog/helper"
 	"shuaibingBlog/routers"
 )
 
@@ -10,11 +12,12 @@ func main() {
 	// Disable Console Color
 	// gin.DisableConsoleColor()
 	e := gin.Default()
+	conf := helper.Config
 	e.HTMLRender = pongo2gin.New(pongo2gin.RenderOptions{
-		TemplateDir: "shuaibingBlog/views",
+		TemplateDir: conf.TemplateDir,
 		ContentType: "text/html; charset=utf-8",
 	})
 	routers.RegisterRouters(e)
 	// Listen and Server in 0.0.0.0:8080
-	e.Run(":8080")
+	e.Run("0.0.0.0:" + conf.Port)
 }
--- utils/mysql.go
+++ utils/mysql.go
@@ -3,14 +3,16 @@ package utils
 import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"shuaibingBlog/helper"
 )
 
 var SqlDB *gorm.DB
 
 func init() {
-	db, err := gorm.Open("mysql", "root:root123@tcp(www.doit05.cn:3306)/yeat?charset=utf8&parseTime=True&loc=Local")
+	conf := helper.Config
+	db, err := gorm.Open("mysql", conf.Mysqlcon)
 	if err != nil {
-		panic("设置Test_db数据库配置失败, err: " + err.Error())
+		panic("设置DB数据库配置失败, err: " + err.Error())
 	}
 	SqlDB = db
 }
