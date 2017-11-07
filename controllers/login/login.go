aa8acd3429aa2f87c190166884952a623d178520
zhanshuaibing <zhanshuaibing@douyu.tv>
2017-11-07 10:40:18 +0800

    合并master

--- controllers/login/login.go
+++ controllers/login/login.go
@@ -1,13 +1,14 @@
 package login
 
 import (
+	"github.com/flosch/pongo2"
 	"github.com/gin-gonic/gin"
 	"shuaibingBlog/helper/apicode"
 )
 
 func Login(c *gin.Context) {
-	c.String(200, "pong")
+	data := pongo2.Context{}
+	data["site"] = c.Request.Host
+	c.HTML(apicode.Success, "login.html", data)
 
 }
-
-
